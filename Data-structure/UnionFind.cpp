e1894653d0f88a9011f1245159792b70d0952084
ItsmeLee <arsmagica90@gmail.com>
2022-07-27 08:37:07 +0900

    Update UnionFind

---
+++ Data-structure/UnionFind.cpp
@@ -0,0 +1,32 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+#define int int64_t
+
+struct UnionFind { // union-by-rank ver.
+	vector<int> par, rank, cnt;
+	UnionFind(int n) : par(n), rank(n, 1), cnt(n, 1) {
+		iota(par.begin(), par.end(), 0);
+	}
+	int Find(int x) {
+		return x == par[x] ? x : par[x] = Find(par[x]);
+	}
+	bool Union(int a, int b) {
+		a = Find(a), b = Find(b);
+		if (a == b) return 0;
+		if (rank[a] < rank[b]) swap(a, b);
+		par[b] = a;
+		rank[a] += rank[a] == rank[b];
+		cnt[a] += cnt[b];
+		return 1;
+	}
+};
+
+int32_t main(){
+    fastio;
+    int n; cin >> n;
+    UnionFind UF(n + 1); // 1-indexed
+}
