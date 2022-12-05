5e2c6ddf92d17698b7aec3f793de0ab3eca152c9
ItsmeLee <arsmagica90@gmail.com>
2022-12-05 15:26:43 +0900

    Update Sieve tech

--- Math/SieveTech.cpp
+++ Math/SieveTech.cpp
@@ -14,32 +14,39 @@ int sieve[sz + 1];
 /* 
 reference : https://rebro.kr/96
 1. 각 2부터 n까지의 자연수 k에 대해, "k가 갖는 가장 작은 소인수"를 계산하는 방법
- */
-void Sieve(){
-    for(int i = 1; i <= sz; i++) sieve[i] = i;
-    for(int i = 2; i <= sz; i++){
-        if(sieve[i] == i){ // 아직 걸러지지 않음. i는 prime number
-            for(int j = 2; j * i <= sz; j++){
-                if(sieve[i * j] == i * j) sieve[i * j] = i;
-            }
-            p.push_back(i);
-        }
-    }
-}
 
-/* 
 2. 각 2부터 n까지의 자연수 k에 대해, "빠르게 k를 소인수분해" 하는 방법
+Factorize 메소드 참고
 time : O(logK)
  */
-vector<int> FactorizeSieve(int x){
-    vector<int> ret;
-    while(x){
-        ret.push_back(sieve[x]);
-        x /= sieve[x];
-    }
-    sort(ret.begin(), ret.end());
-    return ret;
-}
+template<size_t sz = 100'000>
+struct LinearSieve {
+	vector<int> p;
+	int sieve[sz + 1]; // sieve[i] : IsPrime(i) ? 0 : smallest prime factor
+	int d[sz + 1]; // for factorize
+	LinearSieve() : sieve{} {
+		sieve[0] = sieve[1] = -1;
+		for (int i = 2; i <= sz; i++) {
+			if (!sieve[i]) p.push_back(i);
+			for (auto j : p) {
+				if (i * j > sz) break;
+				sieve[i * j] = j;
+				d[i * j] = i;
+				if (i % j == 0) break;
+			}
+		}
+	}
+	bool IsPrime(int n) { return !sieve[n]; }
+	vector<int> Factorize(int t){ 
+		vector<int> ret;
+		while(sieve[t]){
+			ret.push_back(sieve[t]);
+			t /= d[t];
+		}
+		sort(ret.begin(), ret.end());
+		return ret;
+	}
+}; LinearSieve Sieve;
 
 /* 
 3. 약수의 개수 전처리
