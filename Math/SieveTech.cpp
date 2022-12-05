0a35b7e4f86d6cc7c04df979897d49736c3b5b4b
InwooLeeme <arsmagica90@gmail.com>
2022-12-05 15:05:46 +0900

    Update SieveTech.cpp

--- Math/SieveTech.cpp
+++ Math/SieveTech.cpp
@@ -12,11 +12,12 @@ vector<int> p;
 int sieve[sz + 1];
 
 /* 
+reference : https://rebro.kr/96
 1. 각 2부터 n까지의 자연수 k에 대해, "k가 갖는 가장 작은 소인수"를 계산하는 방법
  */
 void Sieve(){
     for(int i = 1; i <= sz; i++) sieve[i] = i;
-    for(int i = 1; i <= sz; i++){
+    for(int i = 2; i <= sz; i++){
         if(sieve[i] == i){ // 아직 걸러지지 않음. i는 prime number
             for(int j = 2; j * i <= sz; j++){
                 if(sieve[i * j] == i * j) sieve[i * j] = i;
