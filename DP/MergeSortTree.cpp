05b6a2efe16f1ffd2ae33fb7b21833342c0dbb38
InwooLeeme <arsmagica90@gmail.com>
2022-12-22 09:46:12 +0900

    Add MergeSortTree

---
+++ DP/MergeSortTree.cpp
@@ -0,0 +1,145 @@
+//////////////////////////////////////////////////////////////////////////////
+/* 
+ _____                            ______                             _        
+/  ___|                           | ___ \                           (_)       
+\ `--.  _ __    __ _   ___   ___  | |_/ /  ___  _ __    __ _  _   _  _  _ __  
+ `--. \| '_ \  / _` | / __| / _ \ |  __/  / _ \| '_ \  / _` || | | || || '_ \ 
+/\__/ /| |_) || (_| || (__ |  __/ | |    |  __/| | | || (_| || |_| || || | | |
+\____/ | .__/  \__,_| \___| \___| \_|     \___||_| |_| \__, | \__,_||_||_| |_|
+       | |                                              __/ |                 
+       |_|                                             |___/     
+//////////////////////////////////////////////////////////////////////////////
+ */
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define X first
+#define Y second
+#define int int64_t
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+#define sf1(a) cin >> a
+#define sf2(a, b) cin >> a >> b
+#define sf3(a, b, c) cin >> a >> b >> c
+#define sf4(a, b, c, d) cin >> a >> b >> c >> d
+#define sf5(a, b, c, d, e) cin >> a >> b >> c >> d >> e
+#define sf6(a, b, c, d, e, f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a, b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a, b, c) cout << (a) << ' ' << (b) << ' ' << (c) << ' '
+#define pf4(a, b, c, d) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' '
+#define pf5(a, b, c, d, e) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' '
+#define pf6(a, b, c, d, e, f) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a, b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a, b, c) cout << (a) << ' ' << (b) << ' ' << (c) << '\n'
+#define pf4l(a, b, c, d) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << '\n'
+#define pf5l(a, b, c, d, e) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << '\n'
+#define pf6l(a, b, c, d, e, f) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' ' << (f) << '\n'
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define pfvec(V)           \
+for (auto const &t : V) \
+pf1(t)
+#define pfvecl(V)           \
+for (auto const &t : V) \
+pf1(t);             \
+pf0l()
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define Init(x, y) memset(x, y, sizeof(x));
+#define EACH(x, a) for (auto& x: a)
+
+using ll  = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tii = tuple<int, int, int>;
+
+using vi = vector<int>;
+using vp = vector<pii>;
+using vvi = vector<vi>;
+using vvp = vector<vp>;
+template <typename T>
+using wector = vector<vector<T>>;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> T max(vector<T> v) { return *max_element(all(v)); }
+
+template<typename T>
+void read(vector<T>& v){
+EACH(i,v) sf1(i);
+}
+
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, 1, -1};
+const int ddx[8] = {0, 0, 1, 1, 1, -1, -1, -1}, ddy[8] = {1, -1, 1, 0, -1, 1, 0, -1};
+const int MOD = 1e9 + 7;
+const double EPS = 1e-10;
+const int INF = 1e9 + 7;
+
+// In 수열과 쿼리 1 : https://www.acmicpc.net/problem/13537
+
+template<typename T = int>
+struct MergeSortTree {
+    int sz;
+    vector<vector<T>> tree;
+    // constructor
+    MergeSortTree(int _sz){this->sz = _sz, tree.resize(_sz << 1);}
+    /* Start Base Method */
+    // 0 - indexed base
+    void Add(int x, T v){
+        x += sz; tree[x].push_back(v);
+    }
+
+    void Construct(const vector<T>& v){
+        const int n = v.size();
+        for(int i = 0; i < n; i++) Add(i, v[i]);
+        for(int i = sz - 1; i; i--){
+            tree[i].resize(tree[i*2].size() + tree[i*2+1].size()); //merge함수를 사용하기 위해서는 공간을 미리 할당해야함
+            merge(all(tree[i*2]), all(tree[i*2+1]), tree[i].begin()); //정렬된 두 개의 배열을 졍렬된 상태로 병합
+        }
+    }
+    /* End Base Method */
+    T Query(int L,int R,int k, int nodeNum , int nodeL,int nodeR){
+        if(R < nodeL || nodeR < L) return 0;
+        if(L <= nodeL and nodeR <= R) return tree[nodeNum].end() - upper_bound(tree[nodeNum].begin(), tree[nodeNum].end(), k);
+        int mid = (nodeL + nodeR) >> 1;
+        return Query(L, R, k, nodeNum * 2 ,nodeL, mid) + Query(L, R, k, nodeNum * 2 + 1, mid + 1, nodeR);
+    }
+    // [l, r] find bigger than k elements
+    T Query(int l,int r,int k){
+        return Query(l, r, k, 1, 1, sz);
+    }
+};
+
+int32_t main(){
+    fastio;
+    int n,q; sf1(n);
+    int sz = 1;
+    while(sz < n) sz <<= 1;
+    vector<int> v(n);
+    read(v);
+    MergeSortTree<int> ST(sz);
+    ST.Construct(v);
+    sf1(q);
+    while(q--){
+        int i,j,k; sf3(i, j, k);
+        pf1l(ST.Query(i, j, k));
+    }
+}
